49770f4e725e2811eef111125e4a8039cffc0099
zwheui <zwheui@gmail.com>
2017-09-07 19:51:46 +0800

    Update footer

--- src/qb-component/QbFooter/QbFooter.js
+++ src/qb-component/QbFooter/QbFooter.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './QbFooter.scss';
 import logo from '../assets/image/logo/light-condensed.png';
 
@@ -9,19 +9,15 @@ class QbFooter extends Component {
             <footer>
                 <ul>
                     <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                        <a href="/blog">Blog</a>
-                    </li>
-                    <li>
                         <a href="/help">Help</a>
                     </li>
                     <li>
                         <a href="mailto:help@quesbook.com?Subject=Help!">Contact us!</a>
                     </li>
                     <li className='logo'>
-                        <img src={logo} alt=""/>
+                        <a href="/home_page">
+                            <img src={logo} alt="" />
+                        </a>
                     </li>
                     <li>
                         <a href="//stg.quesbook.com/start/#/terms/">Terms of use</a>
