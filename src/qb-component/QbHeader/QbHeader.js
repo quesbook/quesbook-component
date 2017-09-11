e6f5ac64997646db33d98e184815516e4021f3e7
Steven <steven.husm@gmail.com>
2017-09-11 15:08:42 +0800

    Add external and interal link in head navi. (#45)

--- src/qb-component/QbHeader/QbHeader.js
+++ src/qb-component/QbHeader/QbHeader.js
@@ -71,6 +71,7 @@ class QbHeader extends Component {
 
     onClick_NavLinkItem(item, e) {
         if (item && item.isRedirect) {
+            e.preventDefault();
             window.location.href = window.location.origin + item.href;
         }
 
@@ -108,13 +109,9 @@ class QbHeader extends Component {
 
         ret = this.state.linkItems.map((item, index) => {
             return (
-                <li key={index} className={item.isActive
-                    ? 'active'
-                    : ''}>
-                    <Link to={item.href} onClick={this.onClick_NavLinkItem.bind(this, item)}>
-                        {item.label}
-                    </Link>
-                </li>
+                item.isRedirect
+                ? this.renderExternalLink(item, index)
+                : this.renderInnerLink(item, index)
             );
         });
 
@@ -133,6 +130,30 @@ class QbHeader extends Component {
                 </div>
             );
         }
+    }
+
+    renderInnerLink(item, index) {
+        return (
+            <li key={index} className={item.isActive
+                ? 'active'
+                : ''}>
+                <Link to={item.href} onClick={this.onClick_NavLinkItem.bind(this, item)}>
+                    {item.label}
+                </Link>
+            </li>
+        ); 
+    }
+
+    renderExternalLink(item, index) {
+        return (
+            <li key={index} className={item.isActive
+                ? 'active'
+                : ''}>
+                <a href='/eclass' onClick={this.onClick_NavLinkItem.bind(this, item)}>
+                    {item.label}
+                </a>
+            </li>
+        ); 
     }
 
     messageToggle() {
