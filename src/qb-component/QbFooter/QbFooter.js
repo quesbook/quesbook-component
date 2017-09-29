6bb588d750d9ca03e3520185f2242c1f4161bc08
zwheui <zwheui@gmail.com>
2017-09-29 09:49:58 +0800

    Update footer css

--- src/qb-component/QbFooter/QbFooter.js
+++ src/qb-component/QbFooter/QbFooter.js
@@ -6,17 +6,17 @@ class QbFooter extends Component {
     render() {
         return (
             <footer>
-                <ul className='box-a-nostyle'>
+                <ul className='box-a-nostyle box-flex'>
+                    <li className='logo'>
+                        <a href="/home_page">
+                            <img src={logo} alt="" />
+                        </a>
+                    </li>
                     <li className='footer-left'>
                         <a href="mailto:help@quesbook.com?Subject=Help!">Help</a>
                     </li>
                     <li className='footer-left'>
                         <a href="mailto:help@quesbook.com?Subject=Help!">Contact</a>
-                    </li>
-                    <li className='logo'>
-                        <a href="/home_page">
-                            <img src={logo} alt="" />
-                        </a>
                     </li>
                     <li className='footer-right'>
                          <a href={`${window.location.origin}/start/#/terms/refundPolicy`}>Refund Policy</a>
