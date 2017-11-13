fc57298c081590eedefb5cffd5cb8d818f08e38b
az <beingszhang@gmail.com>
2017-11-13 10:43:15 +0800

    add a close button to modal

--- src/qb-component/QbModal/QbModal.js
+++ src/qb-component/QbModal/QbModal.js
@@ -2,6 +2,8 @@
  * Created by az on 2017/7/13.
  */
 import React, {Component} from 'react';
+import CloseIcon from '../assets/image/icon/x-icon@3x.png';
+
 /*eslint-disable*/
 class QbModal extends Component {
     constructor(props) {
@@ -19,6 +21,9 @@ class QbModal extends Component {
             <div className="modal fade" id={target} role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
+                        <img alt="closeBtn" src={CloseIcon}
+                            data-target={'#'+target} data-toggle='modal'
+                            style={{position: 'absolute', right: 15, top: 20, height: 15, width: 15}}/>
                         {this.props.children}
                     </div>
                 </div>
