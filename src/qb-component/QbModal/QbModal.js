12b0c5b6eafd89145a099f6f4652349e625eca2d
husm <steven.husm@gmail.com>
2018-04-13 15:20:13 +0800

    [QV-739] Add onClosed event to QBModal component.

--- src/qb-component/QbModal/QbModal.js
+++ src/qb-component/QbModal/QbModal.js
@@ -2,21 +2,32 @@
  * Created by az on 2017/7/13.
  */
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import CloseIcon from '../assets/image/icon/x-icon@3x.png';
 
+function noop() { }
+
+const propTypes = {
+    onClosed: PropTypes.func,
+};
+
+const defaultProps = {
+    onClosed: noop,
+};
 class QbModal extends Component {
     constructor(props) {
         super(props);
-        const {target, afterHidden, afterShown} = this.props;
-        $('#'+ target).on('hidden.bs.modal', function (e) {
-            if (typeof afterHidden === 'function') {
-                afterHidden();
-            }
-        }).on('shown.bs.modal', function (e) {
-            if (typeof afterShown === 'function') {
-                afterShown();
-            }
-        });
+        // const {target, afterHidden, afterShown} = this.props;
+        // $(`#${target}`).on('hidden.bs.modal', function (e) {
+        //     if (typeof afterHidden === 'function') {
+        //         afterHidden();
+        //     }
+        // }).on('shown.bs.modal', function (e) {
+        //     if (typeof afterShown === 'function') {
+        //         afterShown();
+        //     }
+        // });
+        this.onClosed = this.onClosed.bind(this); 
     }
     render() {
         const {target} = this.props;
@@ -25,7 +36,8 @@ class QbModal extends Component {
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <img alt="closeBtn" src={CloseIcon}
-                            data-target={'#'+target} data-toggle='modal'
+                            data-dismiss='modal' aria-label="Close"
+                            onClick={this.onClosed}
                             style={{position: 'absolute', right: 15, top: 20, height: 15, width: 15}}/>
                         {this.props.children}
                     </div>
@@ -41,6 +53,13 @@ class QbModal extends Component {
             $('#'+ target).modal('hide');
         }
     }
+
+    onClosed() {        
+        this.props.onClosed();
+    }
 }
+
+QbModal.propTypes = propTypes;
+QbModal.defaultProps = defaultProps;
 
 export default QbModal;
