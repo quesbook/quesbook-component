ca431842088f9c15afe5d1429d83b798fc68d5c1
Richard Healy <richard.healy@basekit.com>
2018-05-17 20:54:03 +0800

    CSS changes to deal with large numbers
    For the Score component, it now checks the length
    of the score being passed through (toString().length)
    then applies a class based on that.
    
    Importantly, we had to set the width and the height
    of the circle to ensure that it doesn't become
    malformed with different font / screen sizes.

--- src/qb-component/QbScore/QbScore.js
+++ src/qb-component/QbScore/QbScore.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './QbScore.scss';
 
 // props:  score, style
@@ -6,9 +7,21 @@ import './QbScore.scss';
 // <QbScore score="32"></QbScore>
 // <QbScore score="32" style={{'borderColor': '#b9cff3', 'color': '#b9cff3', 'fontSize': '40px'}}></QbScore>
 class QbScore extends Component {
+
+    componentDidMount() {
+        const el = ReactDOM.findDOMNode(this);
+        const dim = el.getBoundingClientRect();
+
+        el.style.width = `${dim.width}px`;
+        el.style.height = `${dim.width}px`;
+    }
+
     render() {
+        const numbers = ['one', 'two', 'three', 'four'];
+        const numberClass = numbers[('' + this.props.score).length - 1];
+
         return (
-            <div className="section-score" style={this.props.style}>
+            <div className={`section-score ${numberClass}-digits`} style={this.props.style}>
                 <span className="score" style={this.props.styleScore}>
                     {this.props.score}
                 </span>
